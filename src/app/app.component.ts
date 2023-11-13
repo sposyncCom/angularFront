9d2eb0401da7e77779db720c67669d2ad9b05251
sposyncSupport <support@sposync.com>
2023-11-13 11:24:31 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,9 @@
-/*import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'angularFront';
-}*/
-
-import { Component, OnInit } from '@angular/core';
-//import { FormGroup, Validators, FormBuilder } from '@angular/forms'; 
-//import { UserComment } from './models/user-comment.model';
-//import { ApiDataService } from './services/api-data.service';   
-//import {UserService} from './user.service';
-//import {throwError} from 'rxjs';
-//import * as $ from 'jquery';
-//import {Injectable} from '@angular/core';
-//import {HttpClient, } from '@angular/common/http';
-//import { Observable } from 'rxjs';
-
-//import { User } from './user/user';
-//declare var $: any;
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  //providers: [ApiDataService,],
 })
 
 
