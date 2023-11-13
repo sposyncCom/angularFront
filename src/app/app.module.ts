164893e6df62e307f6e12646246a48a72f13e820
sposyncSupport <support@sposync.com>
2023-11-13 11:25:45 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
 import { AppComponent } from './app.component';
-
 import { NavIndexComponent } from './nav-index/nav-index.component';
 import { NavMainComponent } from './nav-main/nav-main.component';
-
 import { ApiDataService } from './services/api-data.service';
-
 import { LoginMainComponent } from './login-main/login-main.component';
-
 import { MainIndexComponent } from './login-main/main-index/main-index.component';
 import { EventRegisterComponent } from './login-main/main-index/event-register/event-register.component';
 import { EventHostComponent } from './login-main/main-index/event-host/event-host.component';
 import { EventSelectComponent } from './login-main/main-index/event-select/event-select.component';
 import { EventJoinComponent } from './login-main/main-index/event-select/event-join/event-join.component';
-
 import { P2pLoginComponent } from './p2p-login/p2p-login.component';
 import { P2pCallComponent } from './p2p-login/p2p-call/p2p-call.component';
 import { P2pLabComponent } from './p2p-login/p2p-lab/p2p-lab.component';
@@ -39,35 +31,30 @@ import {NgxWebstorageModule} from 'ngx-webstorage';
     AppComponent,
     NavIndexComponent,
     P2pLoginComponent,
-
     LoginMainComponent,
-
     MainIndexComponent,
     EventRegisterComponent,
     EventHostComponent,
     EventSelectComponent,
     EventJoinComponent,
-
     P2pCallComponent,
     P2pLabComponent,
     NavMainComponent,
-
     SignUpComponent,
     SetProfileComponent,
     ResetPasswordComponent,
     IndexIndexComponent,
-    FooterComponent,
-    
+    FooterComponent    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-    FormsModule, ReactiveFormsModule,
+    FormsModule, 
+    ReactiveFormsModule,
     HttpClientModule,    
-    NgxWebstorageModule.forRoot(),
-    
+    NgxWebstorageModule.forRoot()    
   ],
+  
   providers: [ ApiDataService, LoginMainComponent,{provide: LocationStrategy, useClass: HashLocationStrategy} ],
 
   bootstrap: [AppComponent]
